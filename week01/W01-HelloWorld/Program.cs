579ac64600e7258ea74b65504a1bb453656fdf39
Christian Peraza <christianperaza2000@gmail.com>
2026-09-01 20:35:47 -0300

    Add first assignments

---
+++ week01/W01-HelloWorld/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is " + DateTime.Now);
+
+DateTime today = DateTime.Today;
+DateTime christmas = new DateTime(today.Year, 12, 25);
+int daysLeftForChristmas = (christmas - today).Days;
+
+Console.WriteLine("There are " + daysLeftForChristmas +  " days until the next Christmas");
