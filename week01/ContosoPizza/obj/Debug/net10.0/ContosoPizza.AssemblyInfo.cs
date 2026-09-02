ee8d7c8e31d9836a16c1bdaf7e8f68f89dcfd27b
Christian Peraza <christianperaza2000@gmail.com>
2026-09-02 20:14:36 -0300

    Create short notes

--- week01/ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ week01/ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21572d6753be03f6c25823ffa05ce999f354012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1cd1e8f4800124effc22ec374320b43d46ff0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
