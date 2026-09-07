da4c99fb415a732207954871624cf93ba1cf7b33
Christian Peraza <christianperaza2000@gmail.com>
2026-09-07 19:44:51 -0300

    Create first web app

--- week01/W01-HelloWorld/obj/Debug/net10.0/W01-HelloWorld.AssemblyInfo.cs
+++ week01/W01-HelloWorld/obj/Debug/net10.0/W01-HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W01-HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579ac64600e7258ea74b65504a1bb453656fdf39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8d7c8e31d9836a16c1bdaf7e8f68f89dcfd27b")]
 [assembly: System.Reflection.AssemblyProductAttribute("W01-HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W01-HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
