efb8430ff83cb05ef5cb68f14f29c0b73dd0eb69
Christian Peraza <christianperaza2000@gmail.com>
2026-09-08 19:52:39 -0300

    Add models and database

--- week02/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ week02/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c585eb59b26ceb45e3f2f98a1d4c6f46be90a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c29c3c690d9fc82610c38ded85921340bedbfaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
