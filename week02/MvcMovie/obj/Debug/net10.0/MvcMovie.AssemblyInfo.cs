81b1ce24ceec4fd4ebc706b51b451e20f21f95a5
Christian Peraza <christianperaza2000@gmail.com>
2026-09-09 19:48:07 -0300

    Add validation to mvc movie web app

--- week02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ week02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bea0f045ef6dc721e6b75e5efab1e90413db21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88e91a53aded813a80d1781a63d67580734c9d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
