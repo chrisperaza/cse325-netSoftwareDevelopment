5a2e0622eddaef54ec5bae42f34a216e9f538024
Christian Peraza <christianperaza2000@gmail.com>
2026-09-09 16:21:59 -0300

    Add a view to mvc movie app

--- week02/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ week02/RazorPagesMovie/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae3b204128893a2ab7a52eee2bf7eec13149377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e5b90e6ab97c6a0f64b646312d991889c7395eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
