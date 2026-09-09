fae3b204128893a2ab7a52eee2bf7eec13149377
Christian Peraza <christianperaza2000@gmail.com>
2026-09-09 09:29:56 -0300

    Create mvc movie web app

--- week02/LearningWebApplication/obj/Debug/net10.0/LearningWebApplication.AssemblyInfo.cs
+++ week02/LearningWebApplication/obj/Debug/net10.0/LearningWebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearningWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5c585eb59b26ceb45e3f2f98a1d4c6f46be90a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92168e466d13d989c891a400e60ca1a1d5bf018")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearningWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearningWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
