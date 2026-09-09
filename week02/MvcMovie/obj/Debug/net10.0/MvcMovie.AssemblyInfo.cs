9d6369454b7a721afeb5fdcd6756dfdc628ae845
Christian Peraza <christianperaza2000@gmail.com>
2026-09-09 17:24:51 -0300

    Add database to mvc movie web app

--- week02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ week02/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2e0622eddaef54ec5bae42f34a216e9f538024")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9305ac3c9b22ddeeb5739e3599a9958214881dee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
